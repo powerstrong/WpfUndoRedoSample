b29e0f4117611b4aceb7ab06a37dfa960d12ad2a
jslee <jslee@plaif.com>
2024-09-20 15:18:16 +0900

    add sample models

---
+++ WpfEFTest/Models/JointAngle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEFTest.Models
+{
+    public class JointAngle
+    {
+        [Key]
+        public int Id { get; set; } // 기본 키
+        public double J1 { get; set; }
+        public double J2 { get; set; }
+        public double J3 { get; set; }
+        public double J4 { get; set; }
+        public double J5 { get; set; }
+        public double J6 { get; set; }
+    }
+}
---
+++ WpfEFTest/Models/PrimitiveObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEFTest.Models
+{
+    internal class PrimitiveObject
+    {
+        [Key]
+        public int Id { get; set; } // 기본 키
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+
+    }
+}
